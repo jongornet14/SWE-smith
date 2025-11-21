16cf146c711819891a68d5c4838740133ade5b5c
jongornet14 <jmg1030@nyu.edu>
2025-11-21 17:53:21 -0600

    installation scripts

--- install_scripts/install_repo.sh
+++ install_scripts/install_repo.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-python3 -m venv venv
-source venv/bin/activate
+python3 -m ensurepip --user || true
+python3 -m pip install --user --upgrade pip
 
-pip install --upgrade pip
-pip install -r requirements.txt || true
-pip install .
+python3 -m pip install --user -r requirements.txt || true
+python3 -m pip install --user .
